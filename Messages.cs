16cf5749c012e5593258d1c612f4fddd678cc6b6
Kelsey McDonald <kamcdona@gmail.com>
2020-03-20 14:19:20 -0400

    Add project files.

---
+++ Messages.cs
@@ -0,0 +1,112 @@
+﻿using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+using System.Threading;
+
+namespace HangmanK
+{
+    public class Messages
+    {
+        public static void CallStartMessages()
+        {
+            string[] messages =
+            {
+            @" __    __       ___      .__   __.   _______ .___  ___.      ___      .__   __.
+|  |  |  |     /   \     |  \ |  |  /  _____||   \/   |     /   \     |  \ |  |
+|  |__|  |    /  ^  \    |   \|  | |  |  __  |  \  /  |    /  ^  \    |   \|  |
+|   __   |   /  /_\  \   |  . `  | |  | |_ | |  |\/|  |   /  /_\  \   |  . `  |
+|  |  |  |  /  _____  \  |  |\   | |  |__| | |  |  |  |  /  _____  \  |  |\   |
+|__|  |__| /__/     \__\ |__| \__|  \______| |__|  |__| /__/     \__\ |__| \__|",
+
+            @"  _______      ___      .___  ___.  _______      ______   ____    ____  _______ .______       __ 
+ /  _____|    /   \     |   \/   | |   ____|    /  __  \  \   \  /   / |   ____||   _  \     |  |
+|  |  __     /  ^  \    |  \  /  | |  |__      |  |  |  |  \   \/   /  |  |__   |  |_)  |    |  |
+|  | |_ |   /  /_\  \   |  |\/|  | |   __|     |  |  |  |   \      /   |   __|  |      /     |  |
+|  |__| |  /  _____  \  |  |  |  | |  |____    |  `--'  |    \    /    |  |____ |  |\  \----.|__|
+ \______| /__/     \__\ |__|  |__| |_______|    \______/      \__/     |_______|| _| `._____|(__)",
+
+            @"____    __    ____  __  .__   __. .__   __.  _______ .______      
+\   \  /  \  /   / |  | |  \ |  | |  \ |  | |   ____||   _  \     
+ \   \/    \/   /  |  | |   \|  | |   \|  | |  |__   |  |_)  |    
+  \            /   |  | |  . `  | |  . `  | |   __|  |      /     
+   \    /\    /    |  | |  |\   | |  |\   | |  |____ |  |\  \----.
+    \__/  \__/     |__| |__| \__| |__| \__| |_______|| _| `._____|"
+        };
+
+            string[] counting =
+                {
+            @" __ 
+/_ |
+ | |
+ | |
+ | |
+ |_|",
+            @" ___  
+|__ \ 
+   ) |
+  / / 
+ / /_ 
+|____|",
+            @" ____  
+|___ \ 
+  __) |
+ |__ < 
+ ___) |
+|____/",
+            @" _  _   
+| || |  
+| || |_ 
+|__   _|
+   | |  
+   |_| ", @" _____ 
+| ____|
+| |__  
+|___ \ 
+ ___) |
+|____/"
+        };
+
+            Console.BackgroundColor = ConsoleColor.DarkCyan;
+            Console.Clear();
+
+            for (int i = counting.Length; i > 0; i--)
+            {
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.WriteLine(messages[0]);
+                Console.WriteLine(counting[i - 1]);
+                Thread.Sleep(1000);
+                Console.Clear();
+            }
+
+            Console.ResetColor();
+        }
+
+        public static void CallGameMessages()
+        {
+            Console.Clear();
+            Console.WriteLine("Guess the word: {0}", new string(maskStartWord));
+            Console.WriteLine("Guessed characters: {0}", guessedCharactersList);
+            Console.WriteLine("You have {0} tries left.", guessingTries);
+            Console.WriteLine();
+            Console.Write("Your next guess is: ");
+        }
+
+        public static void CallCharMessages()
+        {
+            Console.WriteLine("You already guessed this letter, try another!");
+            Console.WriteLine("Press enter to continue.");
+            Console.ReadLine();
+        }
+
+        public static void CallViolationMessages()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("\nOnly ONE single character allowed!");
+            Console.WriteLine("You will lose 2 tries for each following violation of this rule.");
+            Console.ResetColor();
+            Console.WriteLine("Press enter to continue.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+//using HangmanK.Messages;
+using System.Threading;
+
+namespace HangmanK
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool gameOver = false;
+            
+            string hangManWord = "guess";
+            string currentGuessedCharacter = string.Empty;
+            string guessedCharactersList = string.Empty;
+
+            char[] maskStartWord = new string('-', hangManWord.Length).ToCharArray();
+
+            int triesMultiplier = 2;
+            int guessingTries = hangManWord.Length * triesMultiplier;
+            //int violations = 0;
+
+
+            Console.CursorVisible = false;
+
+            Messages.CallStartMessages();
+
+            while (!gameOver)
+            {
+                Messages.CallGameMessages();
+
+                currentGuessedCharacter = Console.ReadLine().ToString();
+                guessedCharactersList += currentGuessedCharacter[0] + ", ";
+
+                gameOver = true;
+            }
+        }
+    }
+}
